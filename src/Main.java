54f29c27f387a09dd05272d7144171c7ed35d6ca
Panenkova Daria <dariapanenkova@gmail.com>
2025-02-07 17:46:45 +0300

    first change in my branch

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Second commit test");
         System.out.println("Снова мержим");
         System.out.println("Commit problem test");
+
+        System.out.println("Изменение Panenkova");
     }
 }
