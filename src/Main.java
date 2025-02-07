b1c4001e0782c7ab66828d1d4c63a601c0e9a45d
ESpress <mr.podstrelennyy@mail.ru>
2025-02-07 17:33:39 +0300

    Новая проблема на 6 строке

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Создаем проблему");
         System.out.println("Second commit test");
+        System.out.println("Снова мержим");
     }
 }
